1c196104936952a1b2c30a3681b27bb36e844d4e
yogevbd <yogev132@gmail.com>
2019-09-19 16:01:36 +0300

    Add github-release-notes configuration

---
+++ .grenrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "template": {
+      commit: ({ message, url, author, name }) => `- [${message}](${url}) - ${author ? `@${author}` : name}`,
+      issue: "- {{labels}} {{name}} [{{text}}]({{url}})",
+      label: "[**{{label}}**]",
+      noLabel: "closed",
+      group: "\n#### {{heading}}\n",
+      changelogTitle: "# Changelog\n\n",
+      release: "## {{release}} ({{date}})\n{{body}}",
+      releaseSeparator: "\n---\n\n"
+  }
+}
